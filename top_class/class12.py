f551a51f303fcf807ad1d056153ea040aea177f3
Fazli171 <matem3599798@gmail.com>
2025-01-24 08:23:03 +0500

    cars class1

--- top_class/class12.py
+++ top_class/class12.py
@@ -14,11 +14,18 @@ class Car:
     def setNumber(self, new_number):
         self.car_number = new_number
         return self.car_number
+    
+    def __str__(self):
+        return f"{self.car_name} {self.car_number}"
 cars_name = ['Tiko', 'GentrA', 'Cobalt', 'nexia']
 cars_number = ['10A324GD', '20S091Sl', '30E232SL','01W432HK']
 cars = []
 for i in range(4):
-    car = 
+    car = Car(sample(cars_name, k=1)[0],sample(cars_number,k=1)[0])
+    cars.append(car)
+
+for i in cars:
+    print(i)
 
 
 

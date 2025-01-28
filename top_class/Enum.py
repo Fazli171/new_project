ae29a44f6022d711d0e54027e015f0d1eb286761
Fazli171 <matem3599798@gmail.com>
2025-01-28 09:19:34 +0500

    class enum 3

--- top_class/Enum.py
+++ top_class/Enum.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from datetime import datetime
 
 class Gender(Enum):
     Male = 'male'
@@ -31,10 +32,30 @@ class Person :
         return self.fullName
     
 class Pessenger(Person):
-    def __init__(self, fullName, passpordID, gender :Gender):
+    def __init__(self, fullName : str, passpordID : str, gender :Gender):
         super().__init__(fullName, passpordID)
         self.gender = gender
 
-    def get_info(self):
-        return super().get_info() + f"gender {self.gender}"
+    def get_gender(self):
+        return self.gender
 
+    def get_info(self):
+        return super().get_info() + f"\ngender {self.gender}"
+
+class Driver(Person):
+    def __init__(self, fullName, passpordID, age):
+        super().__init__(fullName, passpordID)
+        self.age = age
+
+    def get_age(self):
+        return self.age
+    
+    def get_borth_year(self):
+        return datetime.now().year
+    
+    def get_info(self):
+        return super().get_info() + f"/nAge {self.age} \nBorth year {self.get_borth_year()}"
+    
+class Train()
+
+    

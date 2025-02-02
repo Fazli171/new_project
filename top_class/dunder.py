28731025cd675373188c8f428895ce24387841fd
Fazli171 <matem3599798@gmail.com>
2025-02-02 13:27:48 +0500

    dunder1

--- top_class/dunder.py
+++ top_class/dunder.py
@@ -1,6 +1,7 @@
 class Math:
     def __init__(self, emaunt):
         self.emaunt = emaunt
+        self.x = 10
 
     def __str__(self):
         return self.emaunt
@@ -8,6 +9,9 @@ class Math:
     def __repr__(self):
         return f"malumot reprda chiqdi {self.emaunt}"
     
+    def __delattr__(self, name):
+        print( f"{name} axususiyati o'chirildi")
+        super.__delattr__(name)
 mat = Math('200')
-print(mat)   
-print(repr(mat))     
+del mat.x   
+print(mat.x)

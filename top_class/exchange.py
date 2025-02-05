b7013112569c2fdd2aa55cb24aee2881b7fe7c43
Fazli171 <matem3599798@gmail.com>
2025-02-05 23:46:25 +0500

    exchange 4

--- top_class/exchange.py
+++ top_class/exchange.py
@@ -8,7 +8,7 @@ with open('p.json', 'w', encoding='utf-8') as f:
     json.dump(filtered_data, f, indent=2 , ensure_ascii=False)
 data = []
 for i in filtered_data:
-    data.append(f'{i['CcyNm_UZ']}\nSotish kursi: {i['Rate']}\nOlish kursi: {round(float(i['Rate'])*0.97, 2)}\nSana: {i['Date']}\n{'='*20}')
+    data.append(f"{i['CcyNm_UZ']}\nSotish kursi: {i['Rate']}\nOlish kursi: {round(float(i['Rate'])*0.97, 2)}\nSana: {i['Date']}\n{'='*20}")
 
 class Tanzaksiya:
     def course(self):
@@ -18,13 +18,13 @@ class Tanzaksiya:
 
 
     def sell(self):
-        result = int(input(f"Valuta juftligini ko'rsating \nUSD->UZS = 0\nRUB->UZS = 1\nEUR->UZS = 2"))
+        result = int(input("Valuta juftligini ko'rsating \nUSD->UZS = 0\nRUB->UZS = 1\nEUR->UZS = 2"))
         emaut = float(input(f"{filtered_data[result]['CcyNm_UZ']} -> qiymatni kriting"))
         return f"{round((float(filtered_data[result]['Rate']) * 0.97 )* emaut, 2)} uzs"
 
     def buy(self):
-        result = int(input(f"Valuta juftligini ko'rsating \nUZS->USD = 0\nUZS=>RUB = 1\nUZS->EUR = 2"))
-        emaut = float(input(f"UZS -> qiymatni kriting"))
+        result = int(input("Valuta juftligini ko'rsating \nUZS->USD = 0\nUZS=>RUB = 1\nUZS->EUR = 2\n"))
+        emaut = float(input("UZS -> qiymatni kriting"))
         return f"{round(emaut / (float(filtered_data[result]['Rate'])), 2)} {filtered_data[result]['CcyNm_UZ']}"
 
 d = Tanzaksiya()
